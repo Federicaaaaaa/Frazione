75423a4c0c26695e7d2fb6c2d8cc610c3799a5eb
Federicaaaaaa <danger2006.bona@gmail.com>
2024-01-28 16:36:01 +0100

    Aggiungere i file di progetto.

---
+++ Frazione/Frazione.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Frazione
+    {
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+        public Frazione(int num, int den) 
+        {
+            Semplificazione(ref num, ref den);
+            Numeratore = num;
+            Denominatore = den;
+        }
+        public void Semplificazione(ref int num, ref int den)
+        {
+            //MCD
+            int resto;
+            int a = num;
+            int b = den;
+            while(b != 0)
+            {
+                resto = a % b;
+                a = b;
+                b = resto;
+            }
+            int MCD = a;
+            //Semplificare
+            den = den / MCD;
+            num = num / MCD;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numeratore, Denominatore);
+        }
+        public static Frazione Parse(string fraction)
+        {
+            string[] subs = fraction.Split('/');
+            return new Frazione(int.Parse(subs[0]), int.Parse(subs[1]));
+        }
+
+    }
+}
---
+++ Frazione/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione f = new Frazione(5, 25);
+            Console.WriteLine(f);
+
+            Frazione fparse = Frazione.Parse(f.ToString());
+            Console.WriteLine(fparse);
+
+            Console.ReadLine();
+        }
+    }
+}
